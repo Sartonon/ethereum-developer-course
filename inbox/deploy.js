9e0cb5cb93bf71a7f0bce8e0f7ff3267659e5054
Santeri Rusila <santtu_1993@hotmail.com>
2018-02-02 15:32:47 +0200

    First project

---
+++ inbox/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'people fault maximum east gun clip genre planet devote year kind drop',
+  'https://rinkeby.infura.io/5g1jQKGuIyd9zFwAUEom' 
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
